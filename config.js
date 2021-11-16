49d4ed5eb8544a7076e22cd49e70fce3e0a470d2
williamtran207 <83764817+trhuy207@users.noreply.github.com>
2021-11-16 07:47:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Emily à!',
+    introTitle: 'Hello Emily cute!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hủm!?',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'có chứ hí hí :3',
     btnNo: 'Không, mơ hả má',
     question:'Trên thế giới hơn 7 tỉ người mà sao Trần lại có tình cảm với tui nè <3',
     btnReply: 'Gửi cho tui <3',
-    reply: 'Emily tui rất là thích ông, chúng ta iu nhau nhé <33333333333333333333',
+    reply: 'Tui gất thích ông :> hãy là ny tui nhé <33333333333333',
     mess: 'Tui biết mà 🥰. Yêu Emily nhiều nhiều 😘😘',
     messDesc: 'Tối nay rảnh ib nhau nha :3',
     btnAccept: 'Okiiiii lun <3',
